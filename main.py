5a66d46fc97b7818df7070d6927459529ceae8e5
Muhammad Ahsan <136963987+muhammad-ahsan12@users.noreply.github.com>
2024-07-22 22:38:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import streamlit as st
+from langchain.chains import RetrievalQA
 from bs4 import BeautifulSoup
 from langchain.prompts.chat import (ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate)
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import FAISS
 from langchain_groq import ChatGroq
 import requests
@@ -24,22 +24,13 @@ prompt = ChatPromptTemplate.from_messages(messages)
 def main():
     # Set up the Streamlit app interface
     st.title('🦜🔗 Chat With Website')
-    st.markdown("""
-    This chatbot extracts text from a specified website in real time and answers questions about the content provided.
-    You can ask questions related to the website content and get accurate responses based on the extracted data.\n
-    For example, you might ask questions like ***"What is the main topic of this page?"*** or,\n
-    ***"Can you summarize the key points?"***.\n
-    The project repository can be found [on my Github](https://github.com/muhammad-ahsan12/MakTek-internship-Task.git).
-    """)
     st.subheader('Input your website URL, ask questions, and receive answers directly from the website.')
-
     key = st.text_input("Enter the Google API key", type='password')
-    groq_api = st.text_input("Enter the Groq API key", type='password')
-    url = st.text_input("Insert the website URL")
+    url = st.text_input("Insert The website URL")
     user_question = st.text_input("Ask a question (query/prompt)")
 
     if st.button("Submit Query", type="primary"):
-        os.environ['GOOGLE_API_KEY'] = key  # Set the Google API key from user input
+        os.environ['GOOGLE_API_KEY'] = key  # Set the Google API key
         
         # Load HTML content from the URL
         r = requests.get(url)
@@ -55,32 +46,22 @@ def main():
         # Create Google Generative AI embeddings
         embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-        # Create FAISS vector database from the text documents
+        # Create a FAISS vector database from the text documents
         vectordb = FAISS.from_texts(texts=docs, embedding=embeddings)
 
         # Create a retriever from the FAISS vector database
         retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 
         # Use a ChatGroq model for question-answering
-        llm = ChatGroq(model="llama3-70b-8192", groq_api_key=groq_api)
+        llm = ChatGroq(model="llama3-70b-8192", groq_api_key="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")
 
-        # Create a ConversationalRetrievalChain instance from the model and retriever
-        chain = ConversationalRetrievalChain.from_llm(
-            llm,
-            retriever=retriever,
-            return_source_documents=True,
-            chain_type="stuff",
-            verbose=True,
-        )
+        # Create a RetrievalQA instance from the model and retriever
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-        chat_history = []
-
-        # Run the prompt and return the response
-        response = chain({"question": user_question, "chat_history": chat_history})
-
-        # Extract and display the result
-        if 'answer' in response:
-            st.write(response['answer'])
+        # Run the user's question through the RetrievalQA and display the response
+        response = qa.invoke({"query": user_question})
+        if 'result' in response:
+            st.write(response['result'])
         else:
             st.write("Answer not found.")
 
