1d1299ce86865a57f0c3e7c024035a5b526411b4
Muhammad Ahsan <136963987+muhammad-ahsan12@users.noreply.github.com>
2024-07-25 01:24:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main():
     #         user_col, _ = st.columns([1, 1])
     #         with user_col:
     #             st.markdown(f"😃 **You:**")
-    #             st.markdown(f"<div style='background-color: #000000; padding: 10px; border-radius: 10px;'>{entry['query']}</div>", unsafe_allow_html=True)
+    #             st.markdown(f"<div style='background-color:#000000; padding: 10px; border-radius: 10px;'>{entry['query']}</div>", unsafe_allow_html=True)
         
               
     #         _, bot_col = st.columns([1, 3])
@@ -117,12 +117,12 @@ def main():
              _, user_col = st.columns([2, 1])
              with user_col:
                  st.markdown(f"😃**You:**")
-                 st.markdown(f"<div style='background-color;#000000 padding: 10px; border-radius: 10px;'>{entry['question']}</div>", unsafe_allow_html=True)
+                 st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['question']}</div>", unsafe_allow_html=True)
 
              bot_col, _ = st.columns([2, 1])
              with bot_col:
                  st.markdown(f"🤖**Bot:**")
-                 st.markdown(f"<div style='background-color;#000000 padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
+                 st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
