85778e8b4a3f8e26d061b5fd3697c9a5416525b1
Muhammad Ahsan <136963987+muhammad-ahsan12@users.noreply.github.com>
2024-07-25 01:24:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -99,30 +99,30 @@ def main():
         # Refresh the page to display the new chat message
         st.experimental_rerun()
 
-    # if st.session_state.chat_history:
-    #     for entry in st.session_state.chat_history:
-    #         user_col, _ = st.columns([1, 1])
-    #         with user_col:
-    #             st.markdown(f"😃 **You:**")
-    #             st.markdown(f"<div style='background-color:#000000; padding: 10px; border-radius: 10px;'>{entry['query']}</div>", unsafe_allow_html=True)
-        
-              
-    #         _, bot_col = st.columns([1, 3])
-    #         with bot_col:
-    #             st.markdown(f"🤖 **Bot:**")
-    #             st.markdown(f"<div style='background-color: #000000; padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
-
     if st.session_state.chat_history:
         for entry in st.session_state.chat_history:
-             _, user_col = st.columns([2, 1])
-             with user_col:
-                 st.markdown(f"😃**You:**")
-                 st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['question']}</div>", unsafe_allow_html=True)
+            user_col, _ = st.columns([1, 1])
+            with user_col:
+                st.markdown(f"😃 **You:**")
+                st.markdown(f"<div style='background-color:#000000; padding: 10px; border-radius: 10px;'>{entry['query']}</div>", unsafe_allow_html=True)
+        
+              
+            _, bot_col = st.columns([1, 3])
+            with bot_col:
+                st.markdown(f"🤖 **Bot:**")
+                st.markdown(f"<div style='background-color: #000000; padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
 
-             bot_col, _ = st.columns([2, 1])
-             with bot_col:
-                 st.markdown(f"🤖**Bot:**")
-                 st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
+    # if st.session_state.chat_history:
+    #     for entry in st.session_state.chat_history:
+    #          _, user_col = st.columns([2, 1])
+    #          with user_col:
+    #              st.markdown(f"😃**You:**")
+    #              st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['question']}</div>", unsafe_allow_html=True)
+
+    #          bot_col, _ = st.columns([2, 1])
+    #          with bot_col:
+    #              st.markdown(f"🤖**Bot:**")
+    #              st.markdown(f"<div style='background-color; #000000 padding: 10px; border-radius: 10px;'>{entry['response']}</div>", unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
